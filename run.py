76cfecba83544328c285a2033d3264691b126dde
lokilal <k_kirill45@mail.ru>
2023-05-24 17:09:58 +0300

    Update requirements, add CORS

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from api.routes import init_route
 from config import DevConfig
 from api.error_handlers import init_handlers
 
 app = Flask('hero')
+CORS(app)
 
 app.config.from_object(DevConfig)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
